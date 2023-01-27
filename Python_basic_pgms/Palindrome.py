f7f4496d6a2e2de4141c18145f0e5b14cc40f912
Subhash <Subhash.n.keerthy@gmail.com>
2023-01-27 08:20:25 +0530

    "Commit again"

--- Python_basic_pgms/Palindrome.py
+++ Python_basic_pgms/Palindrome.py
@@ -1,4 +1,11 @@
 inp = "lool"
 #Method1
 inp = inp.casefold()
-rev_str = 
+rev_str = reversed(str)
+
+if list(inp) = list(rev_str):
+    print("palindrome")
+else:
+    print("not a palindrome")
+
+#Method2
